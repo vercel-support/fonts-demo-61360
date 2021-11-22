38437c87a664d9235cced329ad4a213c337c7b3f
fiiv <git@fiiv.io>
2021-11-22 14:30:49 -0800

    adding in the webpack plugin that hopefully gets things working

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
+const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
+
 module.exports = {
   reactStrictMode: true,
-  experimental: { outputFileTracing: true }
+  experimental: { outputFileTracing: true },
+
+  webpack: function (config, { dev, isServer }) {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.resolve.fallback.fs = false
+    }
+    // copy files you're interested in
+    if (!dev) {
+      config.plugins.push(
+        new CopyPlugin({
+          patterns: [{ from: 'fonts', to: 'fonts' }]
+        })
+      )
+    }
+
+    return config
+  }
 }
