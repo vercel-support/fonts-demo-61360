7afbb7592723c849dc9c0b2b183b365eac95ad81
fiiv <git@fiiv.io>
2021-11-22 14:21:01 -0800

    The Lannisters send their regards

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,27 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import sharp from 'sharp'
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const demoSVG = `
+<svg width="150" height="150" viewBox="0 0 150 150">
+  <style type="text/css">
+    @font-face {
+      font-family: Inconsolata;
+      src: './inconsolata.ttf';
+    }
+  </style>
+  <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+    <rect fill="#ffffff" x="0" y="0" width="150" height="150"></rect>
+    <text font-family="Inconsolata" font-size="200" font-weight="400" fill="#ff0000">
+      <tspan x="50%" y="90%" width="150" text-anchor="middle">V</tspan>
+    </text>
+  </g>
+</svg>`
+
+const handler = async (req, res) => {
+  const source = Buffer.from(demoSVG)
+  const image = await sharp(source).png().toBuffer()
+
+  res.setHeader('Content-Type', 'image/png')
+  res.send(image)
 }
+
+export default handler
