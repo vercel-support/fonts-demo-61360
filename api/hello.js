496bd7b846e2ccb5449884a0aabc468ba4dc34bb
Charles <thexrayone@icloud.com>
2021-11-25 18:23:15 -0500

    trying nft

---
+++ api/hello.js
@@ -0,0 +1,33 @@
+import { readFileSync } from 'fs';
+import path from 'path';
+import sharp from 'sharp';
+
+const fontPath = path.join(process.cwd(), 'fonts', 'inconsolata.ttf');
+
+const handler = async (req, res) => {
+  console.log('__font_path__', fontPath);
+
+  const demoSVG = `
+<svg width="150" height="150" viewBox="0 0 150 150">
+  <style type="text/css">
+    @font-face {
+      font-family: Inconsolata;
+      src: ${fontPath};
+    }
+  </style>
+  <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+    <rect fill="#ffffff" x="0" y="0" width="150" height="150"></rect>
+    <text font-family="Inconsolata" font-size="200" font-weight="400" fill="#ff0000">
+      <tspan x="50%" y="90%" width="150" text-anchor="middle">V</tspan>
+    </text>
+  </g>
+</svg>`;
+
+  const source = Buffer.from(demoSVG);
+  const image = await sharp(source).png().toBuffer();
+
+  res.setHeader('Content-Type', 'image/png');
+  res.send(image);
+};
+
+export default handler;
